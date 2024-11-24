b1d87d14ea1094197ce10a3003a0e1ea7f2afada
Identiddycrysis <ollytrower08@gmail.com>
2024-11-24 18:12:41 +0000

    multiple pages and others

--- app.py
+++ app.py
@@ -5,6 +5,15 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('index.html')
+@app.route('/price_and_availability')
+def price():
+    return render_template('priceandavailability.html')
+@app.route('/reviews')
+def reviews():
+    return render_template('reviews.html')
+@app.route('/gettinghere')
+def gettinghere():
+    return render_template('gettinghere.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
